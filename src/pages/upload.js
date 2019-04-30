2441332ffcf269851388ee4fa864dbd086b8dc83
Victor Solis <victsoli@Victors-MacBook-Pro.local>
2019-04-30 12:13:13 -0600

    New Upload Server Init

---
+++ src/pages/upload.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Upload from "components/upload/upload/Upload";
+import { Container } from "@hackclub/design-system";
+import Card from "@hackclub/design-system/dist/Card";
+
+const UploadPage = () => (
+    <div className="App">
+    <div className="Card">
+      <Upload />
+    </div>
+  </div>
+)
+
+export default UploadPage
