aa8ba81c2a68bfad0f930343d7b646a0dbe497a3
Victor Solis <victsoli@Victors-MacBook-Pro.local>
2019-04-30 16:48:28 -0600

    Fixed api link

--- src/components/upload/upload/Upload.js
+++ src/components/upload/upload/Upload.js
@@ -73,7 +73,7 @@ class Upload extends Component {
       const formData = new FormData();
       formData.append("file", file, file.name);
 
-      req.open("POST", "http://localhost:8000/upload");
+      req.open("POST", "https://infallible-babbage-51a4f3.netlify.com/upload");
       req.send(formData);
     });
   }
